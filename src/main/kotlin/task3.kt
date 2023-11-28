d36b51851799fe3854791d855ba81c0bcb908926
Ulyana-AA <98107469+Ulyana-AA@users.noreply.github.com>
2023-11-28 08:32:12 +0300

    Update task3.kt

--- src/main/kotlin/task3.kt
+++ src/main/kotlin/task3.kt
@@ -1,6 +1,6 @@
 fun main () {
     print("Введите кол-во пришельцев: ")
-    val x : Int = readLine()!!.toInt()
+    val x : Int = readln().toInt()
     val y = calcChairs(x)
     println("Понадобится $y стульев")
 }
